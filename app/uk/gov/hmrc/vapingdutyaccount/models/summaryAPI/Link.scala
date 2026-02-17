6a7206e449b33a3a06728794ca09f59c73e3e9ce
asadhmrc <232776629+asadhmrc@users.noreply.github.com>
2026-02-17 16:59:01 +0000

    VPD-1325: Link field for `APIResponse`

---
+++ app/uk/gov/hmrc/vapingdutyaccount/models/summaryAPI/Link.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vapingdutyaccount.models.summaryAPI
+
+import play.api.libs.json.{JsString, Json, Writes}
+
+/**
+ * A link object
+ * @param key The key in which this link object will be entered into the links field of the `APIResponse`
+ * @param href The href for this link
+ * @param method The request method expected to be used for this link
+ *
+ * @example
+ * {{{
+ *  // ...
+ *   "links" : {
+ *     "key" : { // <-- instance of link case class
+ *       "href" : "url",
+ *       "method" : "GET"
+ *     }
+ *   }
+ * }}}
+ */
+final case class Link(
+                       key: String,
+                       href: String,
+                       method: RequestMethod
+                     )
+
+/**
+ * The implicit writes for the `Link`, these will ignore the "key"
+ * field on the case class constructor and prevent it from being
+ * added to the resultant JSON payload.
+ */
+final implicit val LinkWrites: Writes[Link] = Writes { link =>
+  Json.obj(
+    "href" -> JsString(link.href),
+    "method" -> JsString(link.method)
+  )
+}
