a567bc82b57c8fd7e0a2cb8bb6d1c8d84eff3ae0
asadhmrc <232776629+asadhmrc@users.noreply.github.com>
2026-02-17 16:59:24 +0000

    VPD-1325: `ContactMethod` enum

---
+++ app/uk/gov/hmrc/vapingdutyaccount/models/summaryAPI/ContactMethod.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vapingdutyaccount.models.summaryAPI
+
+import play.api.libs.json.{JsString, Writes}
+
+/**
+ * An enum containing the type of contact method selected
+ */
+enum ContactMethod {
+  /**
+   * PaperlessPreference.Digital, ContactMethod: True/1
+   */
+  case Email
+  /**
+   * PaperlessPreference.Postal, ContactMethod: False/0
+   */
+  case Post
+}
+
+object ContactMethod {
+  /**
+   * Resolves a boolean to a `ContactMethod` enum value.
+   * @param paperlessPreference The paperless preference
+   * @return `ContactMethod`
+   */
+  def resolve(paperlessPreference: Boolean): ContactMethod = {
+    if (paperlessPreference) ContactMethod.Email else ContactMethod.Post
+  }
+}
+
+given Writes[ContactMethod] = Writes { e =>
+  JsString(e.toString.toUpperCase())
+}
