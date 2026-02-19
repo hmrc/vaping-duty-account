63f0efdee5cc54624da11335467871b5f71fc619
asadhmrc <232776629+asadhmrc@users.noreply.github.com>
2026-02-19 10:12:31 +0000

    VPD-1325: Add missing error to `APIErrors`

--- app/uk/gov/hmrc/vapingdutyaccount/models/summaryAPI/APIError.scala
+++ app/uk/gov/hmrc/vapingdutyaccount/models/summaryAPI/APIError.scala
@@ -19,32 +19,34 @@ package uk.gov.hmrc.vapingdutyaccount.models.summaryAPI
 import play.api.http.Status.*
 import play.api.libs.json.{Json, OFormat}
 
-/**
- * An API Error
- *
- * @param message the message that will be returned in the json body
- * @param code the status that should be sent with this `APIError`
- * @example
- * {{{
- * {
- *   "message" : "Bad request (invalid headers or malformed request)",
- *   "code" : 400
- * }
- * }}}
- */
+/** An API Error
+  *
+  * @param message
+  *   the message that will be returned in the json body
+  * @param code
+  *   the status that should be sent with this `APIError`
+  * @example
+  *   {{{
+  * {
+  *   "message" : "Bad request (invalid headers or malformed request)",
+  *   "code" : 400
+  * }
+  *   }}}
+  */
 final case class APIError(message: String, code: Int)
 
-/**
- * Types extracted from OpenAPI specification
- *
- * @example
- * {{{
- * {
- *   "message" : "Bad request (invalid headers or malformed request)",
- *   "code" : 400
- * }
- * }}}
- */
+/** Types extracted from OpenAPI specification
+  *
+  * @example
+  *   {{{
+  * Json.toJson(APIErrors.BadRequest) would serialise to:
+  *
+  * {
+  *   "message" : "Bad request (invalid headers or malformed request)",
+  *   "code" : 400
+  * }
+  *   }}}
+  */
 object APIErrors {
   val BadRequest = APIError("Bad request (invalid headers or malformed request)", BAD_REQUEST)
 
@@ -57,6 +59,8 @@ object APIErrors {
   val InternalServerError = APIError("Internal server error", INTERNAL_SERVER_ERROR)
 
   val ServiceUnavailable = APIError("Service unavailable", SERVICE_UNAVAILABLE)
+
+  val UnprocessableEntity = APIError("Unprocessable entity", UNPROCESSABLE_ENTITY)
 }
 
 implicit val APIErrorFormat: OFormat[APIError] = Json.format[APIError]
