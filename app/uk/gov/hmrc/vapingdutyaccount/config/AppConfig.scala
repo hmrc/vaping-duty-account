b88cafbf8ef7bcb3454322df139dab58fbed4c61
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-03-31 10:58:14 +0100

    VPD-1472: rm unused value

--- app/uk/gov/hmrc/vapingdutyaccount/config/AppConfig.scala
+++ app/uk/gov/hmrc/vapingdutyaccount/config/AppConfig.scala
@@ -95,8 +95,6 @@ class AppConfig @Inject() (config: Configuration, servicesConfig: ServicesConfig
     * Types extracted from OpenAPI specification
     */
   lazy val vpdSummaryRESTAPIEnabled: Boolean = config.get[Boolean]("features.bta.tile.api")
-  /** The pattern that is used to test for valid VpdIds */
-  lazy val vpdIdPattern: String = "(?:GB|XI)WK[0-9]{7}WK"
 
   // Static info for API
   lazy val serviceName: String = config.get[String]("service.name")
