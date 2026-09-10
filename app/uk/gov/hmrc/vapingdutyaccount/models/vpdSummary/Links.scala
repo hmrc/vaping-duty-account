b2c9f44edaca81b4fdbe0039d450d3877a2d84ff
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-09-10 10:48:08 +0100

    VPD-2047: Add ViewPayments to Links

--- app/uk/gov/hmrc/vapingdutyaccount/models/vpdSummary/Links.scala
+++ app/uk/gov/hmrc/vapingdutyaccount/models/vpdSummary/Links.scala
@@ -21,6 +21,7 @@ import play.api.libs.json.{Json, Writes}
 case class Self(href: String, method: String)
 case class ManageContactPreference(href: String, method: String)
 case class MakePayment(href: String, method: String)
+case class ViewPayments(href: String, method: String)
 case class ClaimRepayment(href: String, method: String)
 case class SetUpDirectDebit(href: String, method: String)
 
@@ -29,6 +30,7 @@ implicit val manageContactPreferenceFormats: Writes[ManageContactPreference] = J
 implicit val completeReturnFormats: Writes[CompleteReturn]                   = Json.writes[CompleteReturn]
 implicit val viewReturnsFormats: Writes[ViewReturns]                         = Json.writes[ViewReturns]
 implicit val makePaymentFormats: Writes[MakePayment]                         = Json.writes[MakePayment]
+implicit val viewPaymentFormats: Writes[ViewPayments]                        = Json.writes[ViewPayments]
 implicit val claimRepaymentFormats: Writes[ClaimRepayment]                   = Json.writes[ClaimRepayment]
 implicit val setUpDirectDebitFormats: Writes[SetUpDirectDebit]               = Json.writes[SetUpDirectDebit]
 
@@ -38,6 +40,7 @@ case class Links(
                   completeReturn          : Option[CompleteReturn]          = None,
                   viewReturns             : Option[ViewReturns]             = None,
                   makePayment             : Option[MakePayment]             = None,
+                  viewPayments            : Option[ViewPayments]            = None,
                   claimRepayment          : Option[ClaimRepayment]          = None,
                   setUpDirectDebit        : Option[SetUpDirectDebit]        = None
 )
